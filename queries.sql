b7287d26001da0046e15c30dd406f2e29dce1700
Константин <3304040@gmail.com>
2018-09-25 20:20:15 +0300

    Исправления

--- queries.sql
+++ queries.sql
@@ -1,13 +1,15 @@
+-- Добавление данных
+
 USE yeticave;
-INSERT INTO categories SET name = 'Доски и лыжи';
-INSERT INTO categories SET name = 'Крепления';
-INSERT INTO categories SET name = 'Ботинки';
-INSERT INTO categories SET name = 'Одежда';
-INSERT INTO categories SET name = 'Инструменты';
-INSERT INTO categories SET name = 'Разное';
+INSERT INTO categories SET category_name = 'Доски и лыжи';
+INSERT INTO categories SET category_name = 'Крепления';
+INSERT INTO categories SET category_name = 'Ботинки';
+INSERT INTO categories SET category_name = 'Одежда';
+INSERT INTO categories SET category_name = 'Инструменты';
+INSERT INTO categories SET category_name = 'Разное';
 
 INSERT INTO lots SET creation_date = '2018-09-24 21:10:01',
-name = 'Имя лота',
+lot_name = 'Имя лота',
 description = 'Описание лота',
 image = 'imgage-1.jpg',
 start_price = 100,
@@ -18,7 +20,7 @@ winners_id = 1,
 categories_id = 1;
 
 INSERT INTO lots SET creation_date = '2018-09-22 21:10:01',
-name = 'Имя лота2',
+lot_name = 'Имя лота2',
 description = 'Описание лота2',
 image = 'imgage-2.jpg',
 start_price = 200,
@@ -40,14 +42,41 @@ lots_id = 2;
 
 INSERT INTO users SET registration_date = '2018-09-22 21:10:01',
 email = '3304040@gmail.com',
-name = 'Константин',
+user_name = 'Константин',
 password = '123456',
 avatar = 'avatar.jpg',
 contacts = 'тел 79183304040';
 
 INSERT INTO users SET registration_date = '2018-09-22 21:10:02',
 email = 'serjio@gmail.com',
-name = 'Сергей',
+user_name = 'Сергей',
 password = '7654321',
 avatar = 'avatar-2.jpg',
 contacts = 'тел 7918123456';
+
+-- SQL- запросы
+
+-- получить все категории
+
+SELECT * FROM categories;
+
+/*  получить самые новые, открытые лоты. Каждый лот должен включать название,
+ стартовую цену, ссылку на изображение, цену, количество ставок, название категории */
+
+
+
+
+
+
+--  показать лот по его id. Получите также название категории, к которой принадлежит лот
+
+SELECT creation_date, lot_name, description, image, start_price, end_date, lot_step, category_name FROM lots
+ JOIN categories ON categories.id = lots.id WHERE lots.id = 1;
+
+--  обновить название лота по его идентификатору
+
+UPDATE lots SET lot_name = 'Название лота 3' WHERE id = 1;
+
+--  получить список самых свежих ставок для лота по его идентификатору
+
+SELECT * FROM bet WHERE lots_id = 1 ORDER BY bet_date ASC;
--- schema.sql
+++ schema.sql
@@ -6,13 +6,13 @@ USE yeticave;
 
 CREATE TABLE categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(64)
+  category_name CHAR(64)
 );
 
 CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
   creation_date DATETIME,
-  name CHAR(128),
+  lot_name CHAR(128),
   description TEXT,
   image CHAR(128),
   start_price INT(16),
@@ -22,7 +22,7 @@ CREATE TABLE lots (
   winners_id INT(8),
   categories_id INT(8)
 );
-CREATE INDEX search_lot ON lots(name);
+CREATE INDEX search_lot ON lots(lot_name);
 
 CREATE TABLE bet (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,7 +36,7 @@ CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   registration_date DATETIME,
   email CHAR(64),
-  name CHAR(128),
+  user_name CHAR(128),
   password CHAR(64),
   avatar CHAR(128),
   contacts TEXT
