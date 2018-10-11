9985b379e7dcb2c1ed81250efc03c08836c2b728
Константин <3304040@gmail.com>
2018-10-11 23:19:50 +0300

    Заменяет переменную вывода цены

--- templates/lot.php
+++ templates/lot.php
@@ -19,7 +19,7 @@
                     <div class="lot-item__cost-state">
                         <div class="lot-item__rate">
                             <span class="lot-item__amount">Текущая цена</span>
-                            <span class="lot-item__cost"><?= htmlspecialchars($lot_data['start_price']); ?></span>
+                            <span class="lot-item__cost"><?= htmlspecialchars($current_price); ?></span>
                         </div>
                         <div class="lot-item__min-cost">
                             Мин. ставка <span><?= htmlspecialchars($lot_data['lot_step']); ?></span>
