bfdd07f710fd5cd80890032fdbe50b8746d4d405
Константин <3304040@gmail.com>
2018-10-12 17:12:07 +0300

    Добавляет условие для выключения лота по завершению времени

--- templates/all-lots.php
+++ templates/all-lots.php
@@ -3,7 +3,9 @@
     <section class="lots">
         <h2>Все лоты в категории <span>« »</span></h2>
         <ul class="lots__list">
-            <?php foreach ($lots_list as $key): ?>
+            <?php foreach ($lots_list as $key):
+            $spend_time= strtotime($key['end_date']);
+            if(time() < $spend_time): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
                         <img src="img/<?=$key['image']; ?>" width="350" height="260" alt="">
@@ -16,13 +18,14 @@
                                 <span class="lot__amount">Стартовая цена</span>
                                 <span class="lot__cost"><?=transform_format(htmlspecialchars($key['start_price'])); ?></span>
                             </div>
-                            <div class="lot__timer timer"><?=time_to_end($key['creation_date'], $key['end_date']) ?>
+                            <div class="lot__timer timer"><?=time_to_end($key['end_date']); ?>
 
                             </div>
                         </div>
                     </div>
                 </li>
-            <?php endforeach; ?>
+            <?php endif;
+            endforeach; ?>
 
         </ul>
     </section>
