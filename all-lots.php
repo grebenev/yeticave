6cf9b88038da6e743e28de4ac15e143856efcfb7
Константин <3304040@gmail.com>
2018-10-13 09:47:17 +0300

    Добавляет запрос в БД для вывода текущей категории

--- all-lots.php
+++ all-lots.php
@@ -3,26 +3,31 @@
     require_once('db.php');
     require_once('functions.php');
 
-    $title = 'Все лоты';
-
-
     //вызовы функции
     $categories_list = get_data_db($link, $categories_sql, 'list');
     $content = include_template('all-lots.php', compact('categories_list'));
 
     // ждем запроса с id категории
     $category_id = $_GET['category'] ?? 1;
-    $cur_page = $_GET['page'] ?? 1; // если данных не пришло page = 1
+    $cur_page = intval($_GET['page'] ?? 1); // если данных не пришло page = 1
     $page_items = 6; // кол-во лотов на странице
 
 
     if ($category_id) {
-        //общее количество лотов
+        // добываем нужное нам имя из таблицы категорий
+        $category_result = mysqli_query($link, 'SELECT category_name FROM categories WHERE id ='.$category_id.'');
+        $category_name = mysqli_fetch_assoc($category_result)['category_name'];
+        $title = 'Все лоты в категории «'.$category_name.'»';
+
+        //сапрос общего количества лотов в таблице
         $result = mysqli_query($link, 'SELECT COUNT(*) as count_lots FROM lots WHERE categories_id ='.$category_id.'');
-        // в переменную $items_count записали общее количество записей в lots
+
+        // в переменную $items_count записали общее количество записей в в алиасе count_lots
         $items_count = mysqli_fetch_assoc($result)['count_lots'];
+
         // делим общее число записей на число страниц резкльтат округляем и присваиваем $pages_count
         $pages_count = ceil($items_count / $page_items);
+
         // вычисляем смещение это текущая страница - 1 * на число лотов на странице (6)
         $offset = ($cur_page - 1) * $page_items;
 
@@ -41,7 +46,7 @@ JOIN categories ON categories.id = lots.categories_id WHERE lots.categories_id =
         $pages = range(1, $pages_count);
 
         if (count($lots_list) > 0) {
-            $content = include_template('all-lots.php', compact('lots_list', 'categories_list', 'pages', 'pages_count', 'cur_page', 'category_id'));
+            $content = include_template('all-lots.php', compact('lots_list', 'categories_list', 'pages', 'pages_count', 'cur_page', 'category_id', 'category_name'));
         } else {
             $error = 'Нет такой категории';
         }
