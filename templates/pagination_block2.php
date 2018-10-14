27f91a70e16ec9ea5610d9dc945477e6d227dd5b
Константин <3304040@gmail.com>
2018-10-14 15:00:04 +0300

    Добавляет блок пагинации - потом будет переделываться

---
+++ templates/pagination_block2.php
@@ -0,0 +1,13 @@
+<?php if ($pages_count > 1): ?>
+<ul class="pagination-list">
+    <li class="pagination-item pagination-item-prev"><?php if ($cur_page > 1): ?><a
+            href="?search=<?=$search;?>&page=<?=$cur_page - 1;?>"><?php endif; ?>Назад</a></li>
+    <?php foreach ($pages as $page): ?>
+    <li class="pagination-item <?php if ($page == $cur_page): ?>pagination-item-active<?php endif; ?>">
+        <a <?php if ($page != $cur_page): ?>href="?search=<?=$search;?>&page=<?=$page;?>"<?php endif;
+        ?>><?=$page;?></a></li>
+    <?php endforeach; ?>
+    <li class="pagination-item pagination-item-next"><?php if ($cur_page < $pages_count): ?><a
+            href="?search=<?=$search;?>&page=<?=$cur_page + 1;?>"><?php endif; ?>Вперед</a></li>
+</ul>
+<?php endif; ?>
